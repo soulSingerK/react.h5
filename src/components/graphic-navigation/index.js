36c82bf69de01bafb738730abffc5f2f6d91b402
Gshisan <425139963@qq.com>
2019-01-22 15:14:21 +0800

    feat: add navigator component

--- src/components/graphic-navigation/index.js
+++ src/components/graphic-navigation/index.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
+import './index.scss'
 
 class GraphicNavigation extends React.Component {
+  static propTypes = {
+    data: PropTypes.object.isRequired
+  }
+
   render() {
+    console.log(this.props.data)
+    const businessObj = this.props.data.business_obj
+    const configMap = this.props.data.config_map
     return (
-      <div>图文导航</div>
+      <ul className='navigation-items'>
+        {
+          businessObj.content.map((item, index) => (
+            <li className='navigation-item' key={index}>
+              <a href="javascript: void 0" className='navigation-wrap'>
+                <img src={item.img_url} alt=""/>
+                <p className="navigation-title">{item.title}</p>
+              </a>
+            </li>
+          ))
+        }
+      </ul>
     )
   }
 }
