dd05a972ed5b59539d5386bbe619f20eedf3c629
Gshisan <425139963@qq.com>
2019-01-23 11:43:29 +0800

    feat: activity component

--- src/components/activity/index.js
+++ src/components/activity/index.js
@@ -1,10 +1,59 @@
 import React from 'react'
+import PropTyps from 'prop-types'
+
+import './index.scss'
 
 class Activity extends React.Component {
+  static propTyps = {
+    data: PropTyps.object.isRequired
+  }
+
   render() {
+    const businessObj = this.props.data.business_obj
+    const configMap = this.props.data.config_map
+    const styleType = configMap.style_type
+    const styles = {
+      width: `${businessObj.content.goods_list.length * 106 + 35}px`
+    }
+    
     return (
       <div className="preview-wrap">
-        12121
+        <ul className='activity-wrap' style={styles}>
+          {
+            businessObj.content.goods_list.map((item, index) => (
+              <li
+                className='commodity-item'
+                key={index}
+              >
+                <div className="link">
+                  <div className="img">
+                    <img src={item.pic_url || item.img_url} alt=""/>
+                  </div>
+                  { 
+                    styleType === 'ACTIVITY001' ? 
+                    (
+                      <div className='commodity-wrap'>
+                        <p className='title aqhg-oneline'>{item.title}</p>
+                        <p className='sub-title'>{item.sub_title}</p>
+                        <p className='price'>
+                          {item.price}
+                          <span className='price-origin'>￥{item.retail_price}</span>
+                        </p>
+                      </div>
+                    )
+                    :
+                    (
+                      <div className='commodity-wrap'>
+                        <p className='brand-title aqhg-oneline'>{item.title}</p>
+                        <p className='brand-sub-title'>{item.sub_title}</p>
+                      </div>
+                    )
+                  }
+                </div>
+              </li>
+            ))
+          }
+        </ul>
       </div>
     )
   }
